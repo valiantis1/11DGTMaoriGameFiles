e1776d00da2eb1eb5de494ae0a9450e90f822d53
valiantis1 <valiant.jamison@kingsway.school.nz>
2025-07-17 12:42:52 +1200

    player attack animations
    they work very well now and with clean animations

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -12,8 +12,7 @@ public class PlayerAttack : MonoBehaviour
     [NonSerialized] public bool Attacking;
     private Animator anim;
 
-    private bool AttackUpDownBool;
-    [SerializeField] private float AttackTimeUpDown;
+    private bool AttackUpOrDown;
 
     void Start()
     {
@@ -39,6 +38,11 @@ public class PlayerAttack : MonoBehaviour
               Physics2D.BoxCast(Collider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
               new Vector3(Collider.bounds.size.x * range, Collider.bounds.size.y, Collider.bounds.size.z),
               0, Vector2.left, 0, playerLayer);
+
+            if (hit.collider != null)
+            {
+                hit.collider.enabled = false;
+            }
         }
         else
         {
@@ -46,36 +50,42 @@ public class PlayerAttack : MonoBehaviour
               Physics2D.BoxCast(Collider.bounds.center + -transform.right * range * transform.localScale.x * colliderDistance,
               new Vector3(Collider.bounds.size.x * range, Collider.bounds.size.y, Collider.bounds.size.z),
               0, Vector2.left, 0, playerLayer);
+
+            if (hit.collider != null)
+            {
+                print("working");
+            }
         }
     }
 
     private IEnumerator StartAttacking()
     {
-
         if(playermovement.LeftOrRight)
-            anim.Play("Warrior_Attack1_0");
+        {
+            if(AttackUpOrDown)
+                anim.Play("Warrior_Attack1_0");
+            else
+                anim.Play("Warrior_Attack2_0");
+        }
         else
-            anim.Play("Warrior_Attack1_Left_0");
+        {
+            if(AttackUpOrDown)
+                anim.Play("Warrior_Attack1_Left_0");
+            else
+                anim.Play("Warrior_Attack2_Left_0");
+        }
 
         //plays the attack multiple times to match the animation time.
-        for (int i = 0; i < 4; i++)
+        for (int i = 0; i < 24; i++)
         {
             Attack();
-            yield return new WaitForSeconds(0.1f);
-            print("attack"+i);
+            yield return new WaitForSeconds(0.01f);
+            //print("attack"+i);
         }
         Attacking = false;
         anim.Play("Movement_Blend_Tree");
 
-        AttackUpDownBool = true;
-        StartCoroutine(AttackUpDown());
-    }
-
-    private IEnumerator AttackUpDown()
-    {
-        yield return new WaitForSeconds(AttackTimeUpDown);
-        if (!Attacking)
-            AttackUpDownBool = false;
+        AttackUpOrDown = !AttackUpOrDown;
     }
 
     private void OnDrawGizmos()
