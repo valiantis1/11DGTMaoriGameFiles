3867d5d2170b7d433d1540384ee2a7dc824384aa
valiantis1 <valiant.jamison@kingsway.school.nz>
2025-07-21 13:42:39 +1200

    added Enemy script
    working on added it in more and attack

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,16 +1,23 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
+    private Vector3 target;
+    NavMeshAgent agent; //(this is the AI)
+    public Transform PlayerTrans;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        agent.SetDestination(new Vector3(PlayerTrans.position.x, PlayerTrans.position.y, transform.position.z));
     }
 }
